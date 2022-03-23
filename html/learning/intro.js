44dc68ce617207ee06089c85b9ba22ca0fc79c8c
Arshdhub <lucifer9140@gmail.com>
2022-03-24 02:07:15 +0530

    first commit

---
+++ html/learning/intro.js
@@ -0,0 +1,32 @@
+//print
+console.log("hello i am Arshad Ali");
+//declare variabl
+//eundefind
+let a;
+a=10;
+a=10.1;
+a="hello i am string";
+a='hello i am also string';
+
+a = null;
+a = true;
+console.log("variable contains", a);
+//variable types : primitive type: number , string,boolean, null;
+
+
+
+
+
+
+
+
+
+const person = {
+    name: 'Lydia Hallie',
+    address: {
+      street: '100 Main St',
+    },
+  };
+  
+  Object.freeze(person);
+  
