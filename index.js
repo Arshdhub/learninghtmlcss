05e079a1321eb74b04483e86e5b6281dced183c1
Arshdhub <lucifer9140@gmail.com>
2022-03-24 22:58:05 +0530

    js 1rts par end

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //var,let,constant (variable declare karne ke liye use karte hai)
 
 //variable declaration
-//var a; //int ,string, float ? js me hame type of variablr nhi batana padta hai  
+var a; //int ,string, float ? js me hame type of variablr nhi batana padta hai  
 //variable initalization
 a = 10;
 
@@ -12,7 +12,7 @@ console.log(typeof a);
 a = "hello";
 console.log(a); 
 
-//type of oparator--> it tells us the types of data stored in a variable 
+//typeof oparator--> it tells us the types of data stored in a variable 
 console.log(typeof a);
 
 a = true;
@@ -35,3 +35,46 @@ var t = true;
 var f = false;
 console.log(t);
 console.log(f);
+
+var str = a;
+ console.log(typeof str);
+ str = "How you doing? \ni am fine.how about you?"
+ console.log(str);
+
+ var num = 800
+ console.log(`half of ${num} is ${num/2} `)
+
+
+ // redeclaration
+ var r ="hello"
+ console.log(r)
+
+ var r = 500
+ console.log(r)
+ 
+ //overcome karne ke liye 
+
+ // let key word id used
+
+ let l = 100;
+ console.log(l);
+
+// let l = 200; //SyntaxError: Identifier 'l' has already been declared
+ console.log(l)
+ 
+ // we can reinitialise the variable but we can not redclare
+ l = 300;
+ console.log(l)
+
+// loops in js 
+  var num = 10;
+  for(i=0; i<num; i++){
+      if(i%2==0){
+          console.log("num is even");
+      }
+
+  }
+
+  //CONST KEYWORD
+  const p = 4;
+  console.log(p);
