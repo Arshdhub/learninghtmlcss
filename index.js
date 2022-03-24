c3a5620b693bc6c45968b75ae80ea241833b7c1c
Arshdhub <lucifer9140@gmail.com>
2022-03-24 16:28:39 +0530

    js started

---
+++ index.js
@@ -0,0 +1,24 @@
+// starting JS-> jawa script
+//var,let,constant (variable declare karne ke liye use karte hai)
+
+//variable declaration
+var a; //int ,string, float ? js me hame type of variablr nhi batana padta hai  
+//variable initalization
+a = 10;
+
+console.log(a);
+console.log(typeof a);
+
+a = "hello";
+console.log(a); 
+
+//type of oparator--> it tells us the types of data stored in a variable 
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
